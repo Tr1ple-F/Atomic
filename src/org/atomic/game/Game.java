ee9e25efcdc0518ce0066f5f02e59d3c67f993ca
Triple-F <32393151+Tr1ple-F@users.noreply.github.com>
2019-04-13 11:04:01 +0200

    Base Game architecture

---
+++ src/org/atomic/game/Game.java
@@ -0,0 +1,21 @@
+package org.atomic.game;
+
+/**
+ * Created by Tr1ple-F on the 13.04.2019
+ */
+public class Game {
+
+    public static void init(){
+
+    }
+
+    public static void main(){
+
+        exit();
+    }
+
+    public static void exit(){
+
+    }
+
+}
---
+++ src/org/atomic/game/Launcher.java
@@ -0,0 +1,13 @@
+package org.atomic.game;
+
+/**
+ * Created by Tr1ple-F on the 13.04.2019
+ */
+public class Launcher {
+
+    public static void main(String[] args) {
+        Game.init();
+        Game.main();
+    }
+
+}
